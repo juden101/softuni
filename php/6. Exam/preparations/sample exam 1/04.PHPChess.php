19e51f551c12e58198e032cb14fc7c86a1d6d95a
Иван Захариев <izahariev96@gmail.com>
2014-12-09 15:49:50 +0200

    preparation exam- sample exam 1 done

--- php/6. Exam/preparations/sample exam 1/04.PHPChess.php
+++ php/6. Exam/preparations/sample exam 1/04.PHPChess.php
@@ -1,12 +1,58 @@
 <?php
 
-$_GET['board'] = 'R-H-B-K-Q-B-H-R/P-P- -P-P- -P-P/ - -P- - - - - / - - - - -P- - / - - - - - - - / -P- - - - - -H/P- -P-P-P-P-P-P/R-H-B-K-Q-B- -R';
+$chessBoard = [];
+$figuresCount = [];
+$figureNames = [
+    'B' => 'Bishop',
+    'H' => 'Horseman',
+    'K' => 'King',
+    'P' => 'Pawn',
+    'Q' => 'Queen',
+    'R' => 'Rook',
+    ' ' => ' ',
+];
 
 $board = explode('/', $_GET['board']);
-
 if(count($board) != 8) {
     invalidChessBoard();
 }
+
+foreach($board as $rowCount => $row) {
+    $figures = explode('-', $row);
+
+    if(count($figures) != 8) {
+        invalidChessBoard();
+    }
+
+    foreach($figures as $figureCount => $figure) {
+        if(!array_key_exists($figure, $figureNames)) {
+            invalidChessBoard();
+        }
+
+        $chessBoard[$rowCount][$figureCount] = $figure;
+
+        if($figure != ' ') {
+            if(!isset($figuresCount[$figureNames[$figure]])) {
+                $figuresCount[$figureNames[$figure]] = 0;
+            }
+            $figuresCount[$figureNames[$figure]]++;
+        }
+    }
+}
+
+echo '<table>';
+for($row = 0; $row < count($chessBoard); $row++) {
+    echo '<tr>';
+    for($figure = 0; $figure < count($chessBoard[$row]); $figure++) {
+        echo '<td>' . $chessBoard[$row][$figure] . '</td>';
+    }
+    echo '</tr>';
+}
+echo '</table>';
+
+ksort($figuresCount);
+
+echo json_encode($figuresCount);
 
 function invalidChessBoard() {
     die('<h1>Invalid chess board</h1>');
