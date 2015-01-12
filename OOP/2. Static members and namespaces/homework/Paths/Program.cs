bf94dc947c6e4b30f6e1567ff567d27a581197e2
Иван Захариев <izahariev96@gmail.com>
2015-01-12 22:34:22 +0200

    third task done

--- OOP/2. Static members and namespaces/homework/Paths/Program.cs
+++ OOP/2. Static members and namespaces/homework/Paths/Program.cs
@@ -1,15 +1,40 @@
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Paths
+namespace Geometry
 {
-    class Program
+    class Path
     {
-        static void Main(string[] args)
+        private List<Point> points = new List<Point>();
+
+        static void Main()
         {
+            Point p1 = new Point(1, 2, 3);
+            Point p2 = new Point(3.4, 4.66, 5.55);
+
+            Path path = new Path(p1, p2, Point.StartPoint);
+            Console.WriteLine("Save path: {0}", path);
+            Storage.SavePathInFile("path.txt", path);
+            Path loadPath = Storage.LoadPathOfFile("path.txt");
+            Console.WriteLine("Load path: {0}", loadPath);
+        }
+
+        public Path(params Point[] points)
+        {
+            if (points.Length > 0)
+            {
+                this.points.AddRange(points);
+            }
+        }
+
+        public void AddPoint(Point point)
+        {
+            this.points.Add(point);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(" ", this.points);
         }
     }
 }
