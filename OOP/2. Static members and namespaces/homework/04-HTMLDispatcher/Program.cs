4d35f0ae7094fe2b0a8d944e3236be81f1a9ea7a
Иван Захариев <izahariev96@gmail.com>
2015-01-13 20:00:58 +0200

    fourth task done

---
+++ OOP/2. Static members and namespaces/homework/04-HTMLDispatcher/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ElementBuilder a = new ElementBuilder("a");
+        a.AddAtribute("href", "#");
+        a.AddAtribute("class", "link");
+        a.AddContent("abv.bg");
+
+        Console.WriteLine(a);
+        Console.WriteLine(a * 2);
+        Console.WriteLine();
+
+        Console.WriteLine(HTMLDispecher.CreateImage("image1.jpg", "test image", "test image"));
+        Console.WriteLine(HTMLDispecher.CreateInput("submit", "submit", "Submit"));
+        Console.WriteLine(HTMLDispecher.CreateURL("http://www.w3schools.com", "www.w3schools.com", "w3schools.com"));
+    }
+}
