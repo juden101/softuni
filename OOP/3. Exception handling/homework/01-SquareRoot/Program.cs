b77bcd11c014e5eb109af3ef554e6e3acdc053ec
Иван Захариев <izahariev96@gmail.com>
2015-01-17 21:04:38 +0200

    exception handling homework done

---
+++ OOP/3. Exception handling/homework/01-SquareRoot/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int inputNumber = int.Parse(Console.ReadLine());
+
+        try
+        {
+            Console.WriteLine(Sqrt(inputNumber));
+        }
+        catch (ArgumentOutOfRangeException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Good bye!");
+        }
+    }
+    public static double Sqrt(double value)
+    {
+        if (value < 0)
+        {
+            throw new System.ArgumentOutOfRangeException("value", "Invalid number!");
+        }
+
+        return Math.Sqrt(value);
+    }
+}
