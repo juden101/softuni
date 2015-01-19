66375930badf51aa06ff4a40fb40415864dd93c7
Иван Захариев <izahariev96@gmail.com>
2015-01-19 22:57:15 +0200

    first task done

---
+++ OOP/4. Other types in OOP/homework/01-GalacticGPS/Location.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+public struct Location
+{
+    public double Latitude { get; set; }
+    public double Longtitude { get; set; }
+    public Planet Planet { get; set; }
+
+    public Location(double latitude, double longtitude, Planet planet) : this()
+    {
+        this.Latitude = latitude;
+        this.Longtitude = longtitude;
+        this.Planet = planet;
+    }
+
+    public override string ToString()
+    {
+        string output = String.Format("{0}, {1} - {2}", this.Latitude, this.Longtitude, this.Planet);
+        return output;
+    }
+}
