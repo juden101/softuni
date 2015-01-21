9d9a24ae266cd0569039fbe4596e1793d2da7da1
Иван Захариев <izahariev96@gmail.com>
2015-01-21 18:24:47 +0200

    second task done

---
+++ OOP/4. Other types in OOP/homework/02-FractionCalculator/Fraction.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Text;
+
+public struct Fraction
+{
+    public long Numerator { get; set; }
+
+    private long denominator;
+    public long Denominator
+    {
+        get
+        {
+            return this.denominator;
+        }
+        set
+        {
+            if (value == 0)
+            {
+                throw new DivideByZeroException("Denominator cannot be 0!");
+            }
+
+            this.denominator = value;
+        }
+    }
+
+    public Fraction(long numerator, long denominator)
+        : this()
+    {
+        this.Numerator = numerator;
+        this.Denominator = denominator;
+    }
+
+    public static Fraction operator +(Fraction fr1, Fraction fr2)
+    {
+        checked
+        {
+            fr1.Numerator *= fr2.Denominator;
+            fr2.Numerator *= fr1.Denominator;
+            long commonDenom = fr1.Denominator * fr2.Denominator;
+
+            return new Fraction(fr1.Numerator + fr2.Numerator, commonDenom);
+        }
+    }
+
+    public static Fraction operator -(Fraction fr1, Fraction fr2)
+    {
+        checked
+        {
+            fr1.Numerator *= fr2.Denominator;
+            fr2.Numerator *= fr1.Denominator;
+            long commonDenom = fr1.Denominator * fr2.Denominator;
+
+            return new Fraction(fr1.Numerator - fr2.Numerator, commonDenom);
+        }
+    }
+
+    public override string ToString()
+    {
+        decimal result = (decimal)this.Numerator / this.Denominator;
+        return result.ToString();
+    }
+}
--- OOP/4. Other types in OOP/homework/02-FractionCalculator/Program.cs
+++ OOP/4. Other types in OOP/homework/02-FractionCalculator/Program.cs
@@ -1,15 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace _02_FractionCalculator
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        try
         {
+            Fraction fraction1 = new Fraction(22, 7);
+            Fraction fraction2 = new Fraction(40, 4);
+            Fraction result = fraction1 + fraction2;
+
+            Console.WriteLine(result.Numerator);
+            Console.WriteLine(result.Denominator);
+            Console.WriteLine(result);
+        }
+        catch (DivideByZeroException exc)
+        {
+            Console.WriteLine(exc.Message);
         }
     }
 }
