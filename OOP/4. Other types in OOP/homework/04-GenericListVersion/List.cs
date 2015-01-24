f6d6d6cc8604ee80937534eca49a179d4bfe21a7
Иван Захариев <izahariev96@gmail.com>
2015-01-24 18:27:35 +0200

    half task done

---
+++ OOP/4. Other types in OOP/homework/04-GenericListVersion/List.cs
@@ -0,0 +1,137 @@
+﻿using System;
+
+public class GenericList<T>
+{
+    const int DefaultCapacity = 16;
+
+    private T[] elements;
+    private int count = 0;
+
+    public GenericList(int capacity = DefaultCapacity)
+    {
+        elements = new T[capacity];
+    }
+
+    public int Count
+    {
+        get
+        {
+            return this.count;
+        }
+        set
+        {
+            this.count = value;
+        }
+    }
+
+    public void Add(T element)
+    {
+        if (count >= elements.Length)
+        {
+            T[] tempElements = this.elements;
+            this.elements = new T[this.elements.Length * 2];
+
+            for(int i = 0; i < tempElements.Length; i++) {
+                this.elements[i] = tempElements[i];
+            }
+        }
+
+        this.elements[count] = element;
+        count++;
+    }
+
+    public T Get(int index)
+    {
+        this.CheckIndex(index);
+
+        T result = elements[index];
+        return result;
+    }
+
+    public void Remove(int index)
+    {
+        this.CheckIndex(index);
+
+        T[] tempElements = new T[this.elements.Length - 1];
+        
+        for (int i = 0, c = 0; i < this.elements.Length; i++)
+        {
+            if(i == index) {
+                continue;
+            }
+
+            tempElements[c] = this.elements[i];
+            c++;
+        }
+
+        this.elements = tempElements;
+        this.Count--;
+    }
+
+    public void Insert(int index, T element)
+    {
+        this.CheckIndex(index);
+
+        T[] tempElements = new T[this.Count + 1];
+
+        for (int i = 0; i < this.Count + 1; i++)
+        {
+            if(i == index) {
+                tempElements[i] = element;
+            }
+            else if(i < index) {
+                tempElements[i] = this.elements[i];
+            }
+            else
+            {
+                tempElements[i] = this.elements[i - 1];
+            }
+        }
+
+        this.elements = tempElements;
+        this.Count++;
+    }
+
+    public void Clear()
+    {
+        elements = new T[DefaultCapacity];
+        this.Count = 0;
+    }
+    /*
+    public T Find(T value)
+    {
+        bool found = false;
+
+        for (int i = 0; i < this.Count; i++)
+        {
+            if(value == this.elements[i]) {
+                
+            }
+        }
+
+        T result = elements[index];
+        return result;
+    }*/
+
+    public T this[int index]
+    {
+        get
+        {
+            if (index < 0 || index >= count)
+            {
+                throw new IndexOutOfRangeException(String.Format("Invalid index: {0}.", index));
+            }
+
+            T result = elements[index];
+            return result;
+        }
+    }
+
+    public void CheckIndex(int index)
+    {
+        if (index < 0 || index >= count)
+        {
+            throw new IndexOutOfRangeException(String.Format("Invalid index: {0}.", index));
+        }
+    }
+}
--- OOP/4. Other types in OOP/homework/04-GenericListVersion/Program.cs
+++ OOP/4. Other types in OOP/homework/04-GenericListVersion/Program.cs
@@ -1,15 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace _04_GenericListVersion
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        GenericList<int> list = new GenericList<int>();
+
+        //adding an element 
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
+
+        //accessing element by index
+        //Console.WriteLine(list.Get(2));
+
+        //removing element by index
+        //list.Remove(1);
+
+        //inserting element at given position
+        //list.Insert(2, 17);
+
+        //clearing the list
+        //list.Clear();
+
+        //finding element index by given value
+        //Console.WriteLine(list.Find(3));
+
+        Console.WriteLine("Number of elements: {0}", list.Count);
+
+        for (int i = 0; i < list.Count; i++)
         {
+            int element = list[i];
+            Console.WriteLine(element);
         }
     }
 }
