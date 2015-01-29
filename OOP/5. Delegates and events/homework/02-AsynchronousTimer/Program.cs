6064270f417db937b30dc204503d31bbc1cb2e70
Иван Захариев <izahariev96@gmail.com>
2015-01-29 17:10:09 +0200

    second task

---
+++ OOP/5. Delegates and events/homework/02-AsynchronousTimer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        AsyncTimer timer1 = new AsyncTimer(Task1, 1000, 10);
+        timer1.Start();
+
+        AsyncTimer timer2 = new AsyncTimer(Task2, 500, 20);
+        timer2.Start();
+    }
+
+    public static void Task1(string str)
+    {
+        Console.WriteLine(str);
+    }
+
+    public static void Task2(string str)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(str);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+    
+}
