9d369bf540b13b142a4e2843447bd6413db9eceb
Иван Захариев <izahariev96@gmail.com>
2015-01-30 21:55:50 +0200

    third task done

---
+++ OOP/5. Delegates and events/homework/03-StudentClass/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var student = new Student("Pesho", 18);
+        student.Name = "Gosho";
+        student.Age = 25;
+    }
+}
