b5ac108fd1560aee03c44e207efc9121bbc33893
Иван Захариев <izahariev96@gmail.com>
2015-03-04 20:35:09 +0200

    Functions and function expressions homework four homework tasks done.

---
+++ advanced javascript/1. Functions and function expressions/homework/01., 02. Function Arguments/app.js
@@ -0,0 +1,28 @@
+﻿function printArgsInfo() {
+    var arguments = printArgsInfo.arguments;
+    
+    for (var argumentIndex in arguments) {
+        var currentArgumentValue = arguments[argumentIndex];
+        var currentArgumentType = typeof currentArgumentValue;
+        
+        if (Array.isArray(currentArgumentValue)) {
+            currentArgumentType = "array";
+        }
+        
+        console.log(currentArgumentValue + " (" + currentArgumentType + ")");
+    }
+    
+    console.log();
+}
+
+printArgsInfo(2, 3, 2.5, -110.5564, false);
+printArgsInfo(null, undefined, "", 0, [], {});
+printArgsInfo([1, 2], ["string", "array"], ["single value"]);
+printArgsInfo("some string", [1, 2], ["string", "array"], ["mixed", 2, false, "array"], { name: "Peter", age: 20 });
+printArgsInfo([[1, [2, [3, [4, 5]]]], ["string", "array"]]);
+
+printArgsInfo.call();
+printArgsInfo.call(this, 2, ["asd"], undefined);
+
+printArgsInfo.apply();
+printArgsInfo.apply(this, [2, ["asd"], undefined]);
---
+++ advanced javascript/1. Functions and function expressions/homework/04. Adding Numbers Using Functions/app.js
@@ -0,0 +1,17 @@
+﻿function add(n) {
+    var f = function (x) {
+        return add(n + x);
+    };
+    
+    f.valueOf = f.toString = function () {
+        return n;
+    };
+    
+    return f;
+}
+
+var addTwo = add(2);
+
+console.log(+addTwo); // 2
+console.log(+addTwo(3)); // 5
+console.log(+addTwo(3)(5)); // 10
