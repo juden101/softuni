18312ec1a942fd8554cdb512e7d30fc19ef3a5e4
Иван Захариев <izahariev96@gmail.com>
2015-04-03 23:15:17 +0300

    Slideshow task done

---
+++ javascript applications/1. jQuery Overview/homework/04. Slideshow/app.js
@@ -0,0 +1,42 @@
+(function() {
+    var index = 1;
+    var intervalInMilliseconds = 5000;
+
+    $(function () {
+        $('#button-left').on('click', previousSlide);
+        $('#button-right').on('click', nextSlide);
+        nextSlide();
+
+        setInterval(function () {
+            $('#button-right').trigger('click');
+        }, intervalInMilliseconds);
+    });
+
+    function previousSlide() {
+        slide('previous');
+    }
+
+    function nextSlide(){
+        slide('next');
+    }
+
+    function slide(position) {
+        $('#images-container').children().hide();
+        $('#image' + index).fadeIn(1000);
+
+        if (position == 'previous') {
+            index--;
+
+            if (index == 0) {
+                index = 3;
+            }
+        }
+        else if (position == 'next') {
+            index++;
+
+            if (index == 4) {
+                index = 1;
+            }
+        }
+    }
+}());
