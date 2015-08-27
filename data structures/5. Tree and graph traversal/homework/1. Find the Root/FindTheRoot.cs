3949d2a3fd8d61a1235eba513a3493a6f8212ad7
ivzb <izahariev96@gmail.com>
2015-08-27 15:50:33 +0300

    tree and graph traversal homework first two tasks done

---
+++ data structures/5. Tree and graph traversal/homework/1. Find the Root/FindTheRoot.cs
@@ -0,0 +1,43 @@
+﻿namespace homework
+{
+    using System;
+    using System.Linq;
+
+    class FindTheRoot
+    {
+        public static void Main()
+        {
+            int numberOfNodes = int.Parse(Console.ReadLine());
+            int numberOfEdges = int.Parse(Console.ReadLine());
+            bool[] hasParent = new bool[numberOfNodes];
+
+            for (int i = 0; i < numberOfEdges; i++)
+            {
+                string currentLine = Console.ReadLine();
+                string[] input = currentLine.Split(' ');
+                int child = int.Parse(input[1]);
+
+                hasParent[child] = true;
+            }
+
+            var parentNodes = hasParent
+                .Select((value, key) => new { value, key })
+                .Where(p => p.value == false)
+                .Select(p => p.key)
+                .ToList();
+
+            if (parentNodes.Count() == 1)
+            {
+                Console.WriteLine(parentNodes[0]);
+            }
+            else if (parentNodes.Count() > 1)
+            {
+                Console.WriteLine("Multiple root nodes!");
+            }
+            else
+            {
+                Console.WriteLine("No root!");
+            }
+        }
+    }
+}
---
+++ data structures/5. Tree and graph traversal/homework/2. RoundDance/RoundDance.cs
@@ -0,0 +1,77 @@
+﻿namespace homework
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class RoundDance
+    {
+        private static new Dictionary<int, List<int>> graph;
+        private static new List<int> visited;
+        private static int currentLength;
+        private static int maxLength;
+
+        static void Main()
+        {
+            int numberOfFriendships = int.Parse(Console.ReadLine());
+            int leadNode = int.Parse(Console.ReadLine());
+
+            graph = ReadGraph(numberOfFriendships);
+            visited = new List<int>();
+            currentLength = 0;
+            maxLength = 0;
+
+            DFS(leadNode);
+
+            Console.WriteLine(maxLength);
+        }
+
+        private static void DFS(int node)
+        {
+            if (!visited.Contains(node))
+            {
+                visited.Add(node);
+                currentLength++;
+
+                if (currentLength > maxLength)
+                {
+                    maxLength = currentLength;
+                }
+
+                foreach (var childNode in graph[node])
+                {
+                    DFS(childNode);
+                }
+
+                currentLength = 1;
+            }
+        }
+
+        private static Dictionary<int, List<int>> ReadGraph(int numberOfFriendships)
+        {
+
+            Dictionary<int, List<int>> graph = new Dictionary<int, List<int>>();
+
+            for (int i = 0; i < numberOfFriendships; i++)
+            {
+                string[] currentFriendship = Console.ReadLine().Split(' ');
+                int first = int.Parse(currentFriendship[0]);
+                int second = int.Parse(currentFriendship[1]);
+                
+                if(!graph.ContainsKey(first))
+                {
+                    graph.Add(first, new List<int>());
+                }
+
+                if (!graph.ContainsKey(second))
+                {
+                    graph.Add(second, new List<int>());
+                }
+
+                graph[first].Add(second);
+                graph[second].Add(first);
+            }
+
+            return graph;
+        }
+    }
+}
