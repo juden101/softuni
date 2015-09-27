e1b1b9224dce10f84ed5e9f4d665f95e681a2f06
ivzb <izahariev96@gmail.com>
2015-09-27 16:20:47 +0300

    recursion homework done

---
+++ algorithms/1. Recursion/homework/01. Tower of Hanoi/TowerOfHanoi.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class TowerOfHanoi
+{
+    private static Stack<int> source = new Stack<int>();
+    private static Stack<int> destination = new Stack<int>();
+    private static Stack<int> spare = new Stack<int>();
+
+    private static int stepsTaken = 0;
+
+    static void Main()
+    {
+        int bottomDisk = Setup();
+
+        MoveDisks(bottomDisk, source, destination, spare);
+
+        Console.WriteLine(stepsTaken);
+    }
+
+    private static void MoveDisks(int bottomDisk, Stack<int> source, Stack<int> destination, Stack<int> spare)
+    {
+        stepsTaken++;
+
+        if (bottomDisk == 1)
+        {
+            destination.Push(source.Pop());
+        }
+        else
+        {
+            MoveDisks(bottomDisk - 1, source, spare, destination);
+            destination.Push(source.Pop());
+            MoveDisks(bottomDisk - 1, spare, destination, source);
+        }
+    }
+
+    private static int Setup()
+    {
+        int diskNumber = int.Parse(Console.ReadLine());
+
+        for (int count = diskNumber; count >= 1; count--)
+        {
+            source.Push(count);
+        }
+
+        return diskNumber;
+    }
+}
---
+++ algorithms/1. Recursion/homework/04. Combinaions without Repetition/CombinationsWithoutRepetition.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class CombinationsWithoutRepetition
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int k = int.Parse(Console.ReadLine());
+        int[] arr = new int[k];
+
+        GenerateCombinations(0, arr, 1, n);
+    }
+
+    private static void GenerateCombinations(int index, int[] arr, int startNum, int endNum)
+    {
+        if (index >= arr.Length)
+        {
+            Print(arr);
+
+            return;
+        }
+
+        for (int i = startNum; i <= endNum; i++)
+        {
+            arr[index] = i;
+            GenerateCombinations(index + 1, arr, i + 1, endNum);
+        }
+    }
+
+    private static void Print(int[] arr)
+    {
+        Console.WriteLine(string.Join(" ", arr));
+    }
+}
