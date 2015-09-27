c5d339acc7bfcf487e425580c0cca05f6272f69e
ivzb <izahariev96@gmail.com>
2015-09-27 16:34:12 +0300

    project initial commit

---
+++ web development/Project/Framework/App.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Framework;
+
+class App {
+    private static $_instance = null;
+
+    /**
+     * @return App
+     */
+    public static function getInstance() {
+        if (self::$_instance == null) {
+            self::$_instance = new App();
+        }
+
+        return self::$_instance;
+    }
+}
