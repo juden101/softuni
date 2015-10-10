d1f616a0a7f12536eb0e113e339bb282822023b8
ivzb <izahariev96@gmail.com>
2015-10-10 17:58:37 +0300

    31-May-2015 exam solved

---
+++ Advanced C#/31-May-2015/02. Target Practice/TargetPractice.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class TargetPractice
+{
+    static void Main()
+    {
+        string[] dimensions = Console.ReadLine().Split();
+        int rows = int.Parse(dimensions[0]);
+        int cols = int.Parse(dimensions[1]);
+
+        string input = Console.ReadLine();
+        Queue<char> inputQ = new Queue<char>(input);
+
+        string[] shot = Console.ReadLine().Split();
+        int shotRow = int.Parse(shot[0]);
+        int shotCol = int.Parse(shot[1]);
+        int shotRadius = int.Parse(shot[2]);
+
+        char[,] matrix = new char[rows, cols];
+
+        int position = -1;
+
+        // fill
+        for (int i = rows - 1; i >= 0; i--)
+        {
+            if (position < 0)
+            {
+                for (int j = cols - 1; j >= 0; j--)
+                {
+                    char currentChar = inputQ.Dequeue();
+                    matrix[i, j] = currentChar;
+                    inputQ.Enqueue(currentChar);
+                }
+
+                position = 1;
+            }
+            else if (position > 0)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    char currentChar = inputQ.Dequeue();
+                    matrix[i, j] = currentChar;
+                    inputQ.Enqueue(currentChar);
+                }
+
+                position = -1;
+            }
+        }
+
+        // shot
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                int deltaRow = i - shotRow;
+                int deltaCol = j - shotCol;
+
+                bool isInRadius = deltaRow * deltaRow + deltaCol * deltaCol <= shotRadius * shotRadius;
+
+                if (isInRadius)
+                {
+                    matrix[i, j] = ' ';
+                }
+            }
+        }
+
+        // fall down
+        for (int i = rows - 1; i >= 0; i--)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                if (matrix[i, j] != ' ')
+                {
+                    continue;
+                }
+
+                int currentRow = i - 1;
+
+                while(currentRow >= 0)
+                {
+                    if (matrix[currentRow, j] != ' ')
+                    {
+                        matrix[i, j] = matrix[currentRow, j];
+                        matrix[currentRow, j] = ' ';
+                        break;
+                    }
+
+                    currentRow--;
+                }
+            }
+        }
+
+        // print
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                Console.Write(matrix[i, j]);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Advanced C#/31-May-2015/04. Olympics Are Coming/OlympicsAreComing.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+class OlympicsAreComing
+{
+    static void Main()
+    {
+        string command;
+
+        Dictionary<string, Dictionary<Athlete, int>> data = new Dictionary<string, Dictionary<Athlete, int>>();
+
+        while ((command = Console.ReadLine()) != "report")
+        {
+            string[] commands = command.Split('|');
+
+            string name = Regex.Replace(commands[0], @"\s+", " ").Trim();
+            string country = Regex.Replace(commands[1], @"\s+", " ").Trim();
+
+            if (!data.ContainsKey(country))
+            {
+                data.Add(country, new Dictionary<Athlete, int>());
+            }
+
+            Athlete currentAthlete = new Athlete()
+            {
+                Name = name
+            };
+
+            if (!data[country].ContainsKey(currentAthlete))
+            {
+                data[country].Add(currentAthlete, 0);
+            }
+
+            data[country][currentAthlete]++;
+        }
+
+        var output = data.OrderByDescending(c => c.Value.Sum(cc => cc.Value));
+
+        foreach (var item in output)
+        {
+            Console.WriteLine("{0} ({1} participants): {2} wins", item.Key, item.Value.Count(), item.Value.Sum(i => i.Value));
+        }
+    }
+
+    public class Athlete : IEquatable<Athlete>
+    {
+        public string Name { get; set; }
+
+        public override int GetHashCode()
+        {
+            return this.Name.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Athlete);
+        }
+
+        public bool Equals(Athlete other)
+        {
+            return other != null && this.Name == other.Name;
+        }
+    }
+}
