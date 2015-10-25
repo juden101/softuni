35db7ab34f08754c2f2ca29c0f55210e428e5088
ivzb <izahariev96@gmail.com>
2015-10-25 18:40:12 +0200

    dynamic programming source added

---
+++ algorithms/4. Dynamic programming/lab/Longest-Increasing-Subsequence/LongestIncreasingSubsequence.cs
@@ -0,0 +1,22 @@
+﻿namespace Longest_Increasing_Subsequence
+{
+    using System;
+
+    public class LongestIncreasingSubsequence
+    {
+        public static void Main()
+        {
+            var sequence = new[] { 3, 14, 5, 12, 15, 7, 8, 9, 11, 10, 1 };
+            var longestSeq = FindLongestIncreasingSubsequence(sequence);
+            Console.WriteLine("Longest increasing subsequence (LIS)");
+            Console.WriteLine("  Length: {0}", longestSeq.Length);
+            Console.WriteLine("  Sequence: [{0}]", string.Join(", ", longestSeq));
+        }
+
+        public static int[] FindLongestIncreasingSubsequence(int[] sequence)
+        {
+            // TODO
+            throw new NotImplementedException();
+        }
+    }
+}
