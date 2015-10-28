8478f44686da97d1b543c41ac8dee1df7bf6dda9
ivzb <izahariev96@gmail.com>
2015-10-28 23:22:38 +0200

    signalr added

---
+++ ASP.NET MVC/Twitter/Twitter.Web/Hubs/NotificationsHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace Twitter.Web.Hubs
+{
+    [HubName("notifications")]
+    public class NotificationsHub : Hub
+    {
+        public void SendNotification(string type, string notification)
+        {
+            this.Clients.Others.receiveNotification(type, notification);
+        }
+    }
+}
